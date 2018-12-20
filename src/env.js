aada609ec58104af25fc1b9b298d2382b8261788
slim <smedini@gmail.com>
2018-12-20 16:11:24 +0100

    0.0.1 - info - initial

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('api:XXX')
+const debug = require('debug')('api:info')
 import {version} from './../package.json'
 
 const ENV = {
@@ -11,11 +11,10 @@ const ENV = {
     DB_PORT: process.env.DB_PORT || 27017,
     DB_USER: process.env.DB_USER || "doudou",
     DB_PWD: process.env.DB_PWD || "masta",
-    DB_COLLECTION: process.env.DB_COLLECTION || "XXX",
+    DB_COLLECTION: process.env.DB_COLLECTION || "info",
     
     NODE_ENV: process.env.NODE_ENV || null,
-    VERSION: version,
-    MORGAN: process.env.MORGAN || ':status :method :url :response-time ms - :res[content-length]',
+    VERSION: version
 }
 
 debug({ENV})
