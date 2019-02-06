758370fbbaaa70cb94b83444805d7f71fbd5d0f8
slim <smedini@gmail.com>
2019-02-06 19:13:07 +0100

    imports

--- src/rest/postInfoRest.js
+++ src/rest/postInfoRest.js
@@ -2,7 +2,7 @@ import {Router, run} from "express-blueforest"
 import {col} from "mongo-registry"
 import ENV from "./../env"
 
-import {setUserIdIn, optionalValidDescription, optionalValidEquivId, optionalValidFragmentId, optionalValidFragmentName, optionalValidFragmentType, validId, validPath, optionalValidSelection, validType, validUser, optionalIds, validMongoId, optionalValidItemIds} from "../validations"
+import {setUserIdIn, optionalValidDescription, optionalValidFragmentId, optionalValidFragmentName, optionalValidFragmentType, validId, validPath, optionalValidSelection, validType, validUser, optionalValidItemIds} from "../validations"
 
 const router = Router()
 
